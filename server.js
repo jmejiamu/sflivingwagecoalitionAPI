d3389dda5499cbb34d41059f8c74cfcd68d3bbad
Jose <jmejiamu@gmail.com>
2019-09-11 15:07:49 -0700

    update

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const bodyParse = require('body-parser');
 const knex = require('knex');
 const cors = require('cors');
 
-const db = knex({ 
-    client: 'pg',
+const db = knex({
+    client: 'mysql',
     connection: {
-      connectString : process.env.DATABASE_URL,
-      ssl: true
+      host : 'xxx.xxx.x.xx', // localhost or IP
+      user : 'root',
+      password : 'xxxxx', 
+      database : 'sflivingwage'
     }
   });
 db.select('*').from('ourcampaigns').then(data => {
     console.log(data);
-}); 
+});
 
 const app = express();
 // this does the same fucntion as body- parse
@@ -25,23 +27,28 @@ app.get('/about', (req, res)=>{
     db.select('*').from('about').then(data => {
         res.send(data);
     });
-    
+
 })
 app.get('/ourcampaigns', (req, res)=>{
     db.select('*').from('ourcampaigns').then(data => {
         res.send(data);
     });
-    
+
+})
+app.get('/joinus', (req, res)=>{
+    db.select('*').from('joinf').then(data => {
+        res.send(data);
+    });
+
 })
 
-app.listen(process.env.PORT || 3000,()=>{
-    console.log(`app is running at port ${proccess.env.PORT}`);
+app.get('/events', (req, res)=>{
+    db.select('*').from('calendarevents').then(data => {
+        res.send(data);
+    });
+
 })
-/** 
- * / --> res = this is working
- *  /signin  --->POST = success/fail
- *  /register --> POST = user
- *  /profile/:userId --> GET = user 
- *  /image --> PUT -->  user
- * 
-*/
+app.listen(3001,()=>{
+    console.log('app is running at port 3001');
+})
+
